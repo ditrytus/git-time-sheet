bd7c26a5f174765c24e4f8d76acce98e0d685173
Jakub Gruszecki <jakub.gruszecki@pearson.com>
2018-03-06 16:23:08 +0100

    Added arguments and saving into original working directory.

--- gts.js
+++ gts.js
@@ -1,12 +1,73 @@
+#!/usr/bin/env node
+'use strict';
+
 const shell = require('shelljs');
 const parse = require('csv-parse');
 var linq = require('linq');
 var moment = require('moment');
 var stringify = require('csv-stringify');
 var fs = require('fs');
+var ArgumentParser = require('argparse').ArgumentParser;
+
+var parser = new ArgumentParser({
+  version: '0.1.0',
+  addHelp:true,
+  description: 'Generates time sheet report from git log.'
+});
+
+parser.addArgument(
+  [ '-a', '--author' ],
+  {
+    help: 'Author of commits. Only commits which author contains this value will be included.',
+    required: true
+  }
+);
+
+parser.addArgument(
+    [ '-l', '--limit' ],
+    {
+      help: "Maximum number of commits that will be inspected. This value limits the amount of processed commits so that the script does not have to inspect entire log every time. Default: 200.",
+      defaultValue: 200
+    }
+  );
+
+parser.addArgument(
+    [ '-p', '--period' ],
+    {
+      help: "Period length for which to generate the report. Default: month.",
+      choices: ['day', 'week', 'month', 'year'],
+      defaultValue: 'month'
+    }
+  );
+
+parser.addArgument(
+    [ '-n', '--periodNumber' ],
+    {
+      help: "Number of period where 0 means the current period, -1 previous one. Ex. -p month -n -2 = two months ago. Default: 0.",
+      defaultValue: 0
+    }
+  );
+
+parser.addArgument(
+    [ '-w', '--workingDayStart' ],
+    {
+      help: "An hour when you usually start work. It is used to calculate duration of first commit in a given day in format HH:mm. Default: 09:00.",
+      defaultValue: '09:00'
+    }
+);
+
+parser.addArgument(
+    [ '-o', '--output' ],
+    {
+      help: "Output CSV file name (with extension). DefauktL out.csv.",
+      defaultValue: 'out.csv'
+    }
+);
+
+var args = parser.parseArgs();
 
 
-var author = "Jakub";
+//var author = "Jakub";
 var limit = 200;
 var repository = "C:\\Users\\Jakub_admin\\Documents\\srw";
 var startDate = moment("2018-02-01").startOf('day');
@@ -15,7 +76,9 @@ var workingHoursStart = moment("09:20", 'HH:mm');
 var workingHoursEnd = moment("17:20", 'HH:mm');
 var outputFile = "out.csv"
 
-var command = "git log --no-walk -" + limit.toString() + " --author-date-order --author=\"" + author + "\" --pretty=format:\"%H;%an;%aI;%s'\"";
+var command = "git log --no-walk -" + limit.toString() + " --author-date-order --author=\"" + args.author + "\" --pretty=format:\"%H;%an;%aI;%s'\"";
+
+var workingDirectory = shell.pwd();
 
 var out = shell
     .cd(repository)
@@ -97,6 +160,7 @@ function processLog(arrays)
         .toArray();
 
     stringify(records, {delimiter: ';'}, function(err, output){
+        shell.cd(workingDirectory);
         fs.writeFile(outputFile, output, (err) => {
             if (err) console.error(err);
         });
